7af846a5ae749efcf257789510e9e3e018a0b653
leonardesulaiman <leonardesulaiman@gmail.com>
2022-06-06 10:47:39 -0700

    task 5

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,19 +6,16 @@
  */
 int main(void)
 {
-
 	char c;
-
 	for (c = 'a'; c <= 'z'; c++)
-	{
-		if (c != 'e' && c != 'q')
 		{
-			putchar(c);
+			if (c != 'e' && c != 'q')
+				{
+					putchar(c);
+				}
 		}
-	}
-		
 	putchar('\n');
 
 	return (0);
-
 }
+
