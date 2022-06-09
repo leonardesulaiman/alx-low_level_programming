3686c964de2b14a4fc408da1082af6f4337e0bef
leonardesulaiman <leonardesulaiman@gmail.com>
2022-06-09 14:20:35 -0700

    removing infinite loop code

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,14 +13,15 @@ int main(void)
 
 	i = 0;
 
-	/**
-	* while (1 < 10);
-	*{
-	*putchar(i);
-	*}
-	*/
-
-	printf("Infinite loop avoided! \\o/\n");
+	/*
+	 *while (i < 10)
+	 *{
+	 *putcher(1);
+	 *}
+	 *
+	 * /
+	
+	 printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
