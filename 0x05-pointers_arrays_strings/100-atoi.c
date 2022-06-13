49ef83c5a3052a54be6f9f5f269a086514250c0f
leonardesulaiman <leonardesulaiman@gmail.com>
2022-06-13 16:50:01 -0700

    task 400

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,32 +1,27 @@
 #include "leonard.h"
 
 /**
- * _atoi - convet a string to an integer
- * @s:input
- *
- * @Return:Always 0
+ * _atoi - check the code for Holberton School students.
+ * @s: pointer char type
+ * Return: Always 0.
  */
 int _atoi(char *s)
 {
-int i;
-int res = 0;
-int sig = 1;
-int brk = 0;
+int negative = 1, i = 0;
+unsigned int numero = 0;
 
-for (i = 0; s[i]!='\0';i++)
+for (i = 0; s[i] != '\0'; i++)
+
 {
-if (s[i]=='-')
-	sig = sig *-1;
-if (s[i]>'0' && s[i]<='9')
-{
-	res = res*10;
-	res = res - ([i] - '0');
-	brk - 1;
-}
-else if(brk == 1)
-	break;
-}
-res = sig*res;
-return(res);
+if (s[i] == '-')
+negative *= -1;
+
+else if (s[i] >= 0 + '0' && s[i] < 10 + '0')
+numero = numero * 10 + (s[i] - '0');
+
+else if (s[i - 1] >= 0 + '0' && s[i - 1] < 10 + '0')
+break;
 }
 
+return (numero *negative);
+}
