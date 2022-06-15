184b05d215cf4bf2ad655648ba18bbd8666d6b2f
leonardesulaiman <leonardesulaiman@gmail.com>
2022-06-15 11:28:12 -0700

    task888

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,5 @@
 #include "leonard.h"
-
+#include <stdio.h>
 /**
  * rot13 - function that encodes a string using rot13.
  * @s: pointer pointed to the variable s , of type character
@@ -11,27 +11,23 @@
 
 char *rot13(char *s)
 {
-int i;
 
-char *p = s;
+int i = 0, i2 = 0;
 
-char *original = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char *code = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char alpha2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-while (*s != '\0')
+while (*(s + i) != 0)
 {
-
-i = 0;
-while (i < 53)
+for (i2 = 0; i2 <= 52; i2++)
 {
-if (*s == original[i])
+if (*(s + i) == alpha[i2])
 {
-*s = code[i]
+*(s + i) = alpha2[i2];
 break;
+}
 }
 i++;
 }
-s++;
-}
-return (p);
+return (s);
 }
